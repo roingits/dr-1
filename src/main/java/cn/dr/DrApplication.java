4fb4dd516ea79ed51e7be2a1e1ca67f96f6f95c5
hahahadd <850586765@qq.com>
2019-06-26 10:16:53 +0800

    启动类添加mapperScan扫描

--- src/main/java/cn/dr/DrApplication.java
+++ src/main/java/cn/dr/DrApplication.java
@@ -1,9 +1,11 @@
 package cn.dr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.dr.mapper")
 public class DrApplication {
 
     public static void main(String[] args) {
