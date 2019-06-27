5764160b7889e5361a3863bc54ff1d937bb44a4c
LovelyAnNaNa <2806121640@qq.com>
2019-06-27 08:34:28 +0800

    添加了负责跳页的Controller

---
+++ src/main/java/cn/dr/controller/DrApplicationController.java
@@ -0,0 +1,36 @@
+package cn.dr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 专门负责跳页的Controller
+ */
+@Controller
+public class DrApplicationController {
+
+    @RequestMapping("/index.html")
+    public String index(){
+        return "index";
+    }
+    
+    @RequestMapping("/lists.html")
+    public String list(){
+        return "lists";
+    }
+    
+    @RequestMapping("/brand.html")
+    public String brand(){
+        return "brand";
+    }
+    
+    @RequestMapping("/detail.html")
+    public String detail(){
+        return "detail";
+    }
+
+    @RequestMapping("/login.html")
+    public String login(){
+        return "login";
+    }
+}
